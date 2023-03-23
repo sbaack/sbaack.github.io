7371d6e64573aabee8dac1453c0016ea8be9514c
sbaack <s.baack@posteo.de>
2023-03-23 11:18:15 +0100

    Remove output retention delete_output_directory settings

--- publishconf.py
+++ publishconf.py
@@ -6,19 +6,16 @@
 
 import os
 import sys
+
 sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://sbaack.com'
+SITEURL = "https://sbaack.com"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
-
-DELETE_OUTPUT_DIRECTORY = True
-
-OUTPUT_RETENTION = [".git", ".gitignore", "CNAME", "favicon.ico"]
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 # Following items are often useful when publishing
 
